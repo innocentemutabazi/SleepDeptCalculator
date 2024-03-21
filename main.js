84842519c9ca5810427b49335f6f6a818a4e99da
innocentemutabazi <innocentemutabazi@gmail.com>
2024-03-21 21:10:16 +0200

    added html and css

--- main.js
+++ main.js
@@ -1,53 +1,32 @@
+const calculateButton = document.getElementById('calculate-button');
+const sleepHoursInputs = document.querySelectorAll('.sleep-hours');
 
-const getSleepHours = day => {
-    if (day === 'monday') {
-        return 8;
-    } else if (day === 'tuesday') {
-        return 7;
-    } else if (day === 'wednesday') {
-        return 4;
-    } else if (day === 'thursday') {
-        return 8;
-    } else if (day === 'friday') {
-        return 6;
-    } else if (day === 'saturday') {
-        return 6;
-    } else if (day === 'sunday') {
-        return 9;
-    }
-};
-console.log(getSleepHours('monday'));
-console.log(getSleepHours('saturday'));
+calculateButton.addEventListener('click', calculateSleepDebt);
 
-const getActualSleepHours = () => {
-    return getSleepHours('monday') +
-     getSleepHours('tuesday') + 
-     getSleepHours('wednesday') + 
-     getSleepHours('thursday') + 
-     getSleepHours('friday') + 
-     getSleepHours('saturday') + 
-     getSleepHours('sunday');
-};
-
-console.log(getActualSleepHours());
-
-const getIdealSleepHours = () => {
+function calculateSleepDebt() {
+    let totalActualSleepHours = 0;
     const idealHours = 8;
-    return idealHours * 7;
-};
 
-console.log(getIdealSleepHours());
+    sleepHoursInputs.forEach(input => {
+        if (input.value) {
+            totalActualSleepHours += parseInt(input.value);
+        }
+    });
 
-const calculateSleepDebt = () => {
-    const actualSleepHours = getActualSleepHours();
-    const idealSleepHours = getIdealSleepHours();
-    
-    if (actualSleepHours === idealSleepHours) {
-        console.log('Congratulations, you actually slept' + (idealSleepHours) + 'just as you wanted to');
-    } else if (actualSleepHours > idealSleepHours) {
-        console.log('You got more sleep than needed. You slept ' + (actualSleepHours - idealSleepHours) + ' hours extra.');
-    } else {
-        console.log('You should get some rest. You slept ' + (idealSleepHours - actualSleepHours) + ' hours less than needed.');
+    const idealSleepHours = idealHours * 7;
+
+    let resultMessage;
+    if (totalActualSleepHours === idealSleepHours) {
+        resultMessage = 'Congratulations, you actually slept ' + idealSleepHours + ' hours, just as you wanted to.';
+    } else if (totalActualSleepHours > idealSleepHours) {
+        resultMessage = 'You got more sleep than needed. You slept ' + (totalActualSleepHours - idealSleepHours) + ' hours extra.';
+    } else if (totalActualSleepHours===0) {
+        resultMessage='You did not get any sleep at all'
     }
-};
-calculateSleepDebt();
+    else {
+        resultMessage = 'You should get some rest. You slept ' + (idealSleepHours - totalActualSleepHours) + ' hours less than needed.';
+    }
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = resultMessage;
+}
